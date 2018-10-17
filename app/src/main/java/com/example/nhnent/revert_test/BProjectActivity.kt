98bfa3b5cc63227e144c5b65e138ac261f5d6700
Kim Hyun Bong <nonark@naver.com>
2018-10-17 15:31:21 +0900

    [b_project] 프로젝트 B 생성

---
+++ app/src/main/java/com/example/nhnent/revert_test/BProjectActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nhnent.revert_test
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BProjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
