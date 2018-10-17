67534f37dd06c14f323a307ecacbf5f66a50297f
Kim Hyun Bong <nonark@naver.com>
2018-10-17 15:58:34 +0900

    [new project] 새로운 값 추가

--- app/src/main/java/com/example/nhnent/revert_test/MainActivity.kt
+++ app/src/main/java/com/example/nhnent/revert_test/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val NEW_VALUE : Int = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
