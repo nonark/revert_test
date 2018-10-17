2463fe02a37bd53cd2443d856331ddb9649b9106
Kim Hyun Bong <nonark@naver.com>
2018-10-17 15:46:53 +0900

    [release] MainActivity 버튼 생성 / 이벤트 추가

--- app/src/main/java/com/example/nhnent/revert_test/MainActivity.kt
+++ app/src/main/java/com/example/nhnent/revert_test/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.nhnent.revert_test
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button : Button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            
+        }
     }
 }
